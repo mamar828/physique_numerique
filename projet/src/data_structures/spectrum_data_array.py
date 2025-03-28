161c7ca1d8aa30061bc1fa7f293e643161f56870
Mathieu Marquis <mamar828@ulaval.ca>
2025-03-28 01:58:02 -0400

    Added params and spectrum to SpectrumDataArray

--- projet/src/data_structures/spectrum_data_array.py
+++ projet/src/data_structures/spectrum_data_array.py
@@ -9,7 +9,7 @@ class SpectrumDataArray:
     This class implements a data array for spectra that uses numpy arrays to store the intensity data.
     """
 
-    def __init__(self, data: np.ndarray) -> None:
+    def __init__(self, data: np.ndarray, params: np.ndarray, spectrum: Spectrum) -> None:
         """
         Initializes a SpectrumDataArray object.
 
@@ -17,8 +17,16 @@ class SpectrumDataArray:
         ----------
         data : np.ndarray
             A size (n,m) numpy array containing n spectra with m channels each.
+        params : np.ndarray
+            A size (n,j,k) numpy array containing the parameters used for creating the data, where n is the number of
+            spectra, j is the number of models and k is the number of parameters in each model.
+        spectrum : Spectrum
+            The Spectrum object used to create the data. This object is used to store the models that generated the data
+            and which correspond to the given params.
         """
         self.data = data
+        self.params = params
+        self.spectrum = spectrum
 
     @classmethod
     def generate_from_spectrum(cls, spectrum: Spectrum, n_spectra: int) -> Self:
@@ -37,5 +45,5 @@ class SpectrumDataArray:
         SpectrumDataArray
             The newly generated SpectrumDataArray object.
         """
-        data = spectrum.evaluate(n_spectra)
-        return cls(data)
+        data, params = spectrum.evaluate(n_spectra)
+        return cls(data, params, spectrum)
