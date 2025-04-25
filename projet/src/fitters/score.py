0f69b9fcdbd5676e6c0ba522b7f6d029175c1315
Mathieu Marquis <mamar828@ulaval.ca>
2025-04-25 17:45:59 -0400

    Faster score.mean_r2_score for spectrums with CustomGaussians

--- projet/src/fitters/score.py
+++ projet/src/fitters/score.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 
 from projet.src.data_structures.spectrum_data_object import SpectrumDataObject
 from projet.src.data_structures.spectrum_dataset import SpectrumDataset
+from projet.src.models.custom_gaussian import CustomGaussian
 
 
 def mean_squared_error(
@@ -84,19 +85,35 @@ def mean_r2_score(
     -------
     float
         The coefficient of determination between the parameters. It converges to 1 when the fitted parameters are close
-        to the true parameters.
+        to the spectrum data.
     """
     if isinstance(spectrum_data, SpectrumDataset):
-        y_true = spectrum_data.data.squeeze(1).numpy()
+        y_true = spectrum_data.data.squeeze(1)
+        fitted_params = fitted_params.numpy()
     else:
         y_true = spectrum_data.data
 
-    y_pred = (
-        spectrum_data.spectrum(spectrum_data.spectrum.x_values, fitted_params_i) for fitted_params_i in fitted_params
-    )           # this creates a generator of the fitted parameters
+    # If spectrum_data.spectrum contains only gaussians, the computation can be greatly accelerated
+    if all(isinstance(model, CustomGaussian) for model in spectrum_data.spectrum.models):
+        # Convert all parameters to 3D arrays so the operations are made for all spectra, for all models and for all
+        # channels
+        x_values = spectrum_data.spectrum.x_values[None,None,:]
+        A = fitted_params[:,:,0][...,None]
+        mu = fitted_params[:,:,1][...,None]
+        sigma = fitted_params[:,:,2][...,None]
 
-    r2_scores = []
-    for y_true_i, y_pred_i in tqdm(zip(y_true, y_pred), f"Computing R^2", len(y_true), colour="RED", unit="fit"):
-        r2_scores.append(r2_score(y_true_i, y_pred_i))
+        gaussian_values = A * np.exp(-((x_values - mu) / sigma) ** 2)
+        y_pred = np.sum(gaussian_values, axis=1)
+        return r2_score(y_true, y_pred, multioutput="uniform_average")
 
-    return np.mean(r2_scores)
+    else:
+        y_pred = (
+            spectrum_data.spectrum(spectrum_data.spectrum.x_values, fitted_params_i) 
+            for fitted_params_i in fitted_params
+        )           # this creates a generator of the fitted parameters
+
+        r2_scores = []
+        for y_true_i, y_pred_i in tqdm(zip(y_true, y_pred), f"Computing R^2", len(y_true), colour="RED", unit="fit"):
+            r2_scores.append(r2_score(y_true_i, y_pred_i))
+
+        return np.mean(r2_scores)
