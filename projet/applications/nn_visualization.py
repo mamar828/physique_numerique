5096cca6307d857692d7d56deb60a523f6f2b583
Mathieu Marquis <mamar828@ulaval.ca>
2025-04-25 17:47:44 -0400

    training/visualization scripts renamed

--- projet/applications/nn_visualization.py
+++ projet/applications/nn_visualization.py
@@ -7,17 +7,17 @@ from projet.src.fitters.score import *
 from projet.src.tools.utilities import show_fit_plot
 
 
-SPEC_FILE = "distinct_gaussians/very_noisy"
-N_SAMPLES = 100
+SPEC_FILE = "distinct_gaussians/smooth"
+N_SAMPLES = 200
 fitter = CNNFitter.load(f"projet/data/neural_networks/CNNFitter/{SPEC_FILE.replace('/', '_')}.pt")
 
 spec = Spectrum.load(f"projet/data/spectra/{SPEC_FILE}.txt")
 dataset = SpectrumDataset.generate_from_spectrum(spec, N_SAMPLES)
 data_loader = DataLoader(dataset, batch_size=1)
 
+
 fits = fitter.predict(data_loader)
 r2 = mean_r2_score(fits, dataset)
 mse = mean_squared_error(fits, dataset.params)
-print(r2, mse)
 
-show_fit_plot(dataset, fits, show_true=True)
+# show_fit_plot(dataset, fits, show_true=False)
